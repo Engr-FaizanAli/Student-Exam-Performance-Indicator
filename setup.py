11a1e04a2b8e9580481ea4b8ecbfbdaf31d91ead
Faizan Ali <you@example.com>
2023-11-15 09:12:15 +0500

    Data Transformatioin completed

--- setup.py
+++ setup.py
@@ -13,10 +13,12 @@ def get_requirements(file_path:str)->List[str]:
 
 setup(
     name='mlproject',
-    version='0.01',
-    author='Faizan Ali'
-    author_email='fali31820@gmail.com'
+    version='0.0.1',
+    author='Faizan Ali',
+    author_email='fali31820@gmail.com',
     packages=find_packages(),
     install_requires=get_requirements('requirements.txt'),
 
 )
+if __name__=='__main__':
+    setup()
---
+++ src/utils.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+import os
+import sys
+from logger import logging
+from exception_handling import CustomException
+import dill
+import pickle
+
+def save_obj(file_path,obj):
+    try:
+        dir_path=os.path.dirname(file_path)
+        os.makedirs(dir_path,exist_ok=True)
+        with open(file_path,"wb") as file_obj:
+            pickle.dump(obj,file_obj)
+    except Exception as e:
+        raise CustomException(e,sys)
