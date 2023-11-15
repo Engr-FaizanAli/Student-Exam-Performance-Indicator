bc4cf9743c0d8205a9812398116707a7533d5b8c
Faizan Ali <you@example.com>
2023-11-15 22:04:06 +0500

    Model Trainer file is done

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def get_requirements(file_path:str)->List[str]:
 
 setup(
     name='mlproject',
-    version='0.0.1',
+    version='0.0.2',
     author='Faizan Ali',
     author_email='fali31820@gmail.com',
     packages=find_packages(),
--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ from logger import logging
 from exception_handling import CustomException
 import dill
 import pickle
+from sklearn.metrics import r2_score
 
 def save_obj(file_path,obj):
     try:
@@ -15,3 +16,17 @@ def save_obj(file_path,obj):
             pickle.dump(obj,file_obj)
     except Exception as e:
         raise CustomException(e,sys)
+def evaluate_model(x_train,y_train,x_test,y_test,models):
+        try:
+            report={}
+            for i in range(len(list(models))):
+                model=list(models.values())[i]
+                model.fit(x_train,y_train)
+                y_train_pred=model.predict(x_train)
+                y_test_pred=model.predict(x_test)
+                train_model_score=r2_score(y_train,y_train_pred)
+                test_model_score=r2_score(y_test,y_test_pred)
+                report[list(models.keys())[i]]=test_model_score
+                return report
+        except:
+            pass
